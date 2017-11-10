6c229ca38f5f79cab305ac1bff7ce0138f9c13a5
unknown <309425239@qq.com>
2017-11-10 21:43:51 +0800

    管理员登录功能完善、管理员个人设置完成、网站信息展示完成

---
+++ js/cookie.js
@@ -0,0 +1,25 @@
+//封装函数
+//username=666 expires=xxx pwd=666;
+function setCookie(key,value,time){
+	var oDate = new Date();
+	oDate.setDate(oDate.getDate()+time);
+	document.cookie = key+'='+encodeURI(value)+';expires='+oDate.toGMTString();
+}
+//获取cookie
+function getCookie(key){
+	var arr1 = document.cookie.split(';');
+	for(var i=0;i<arr1.length;i++){
+		var arr2 = arr1[i].split('=');
+		if(arr2[0]==key){
+			return decodeURI(arr2[1]);
+		}
+	}
+}
+//删除cookie
+function removeCookie(key){
+	setCookie(key,'',-1);
+}
+$('#name').text(getCookie('username'));
+$('#logout').on('click',function(){
+	removeCookie('username');
+});
