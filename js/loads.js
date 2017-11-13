1fecbe321d1336655a4cc86d853702827eecf9d1
Snow <1011341509@qq.com>
2017-11-13 09:56:04 +0800

    后台管理--用户信息

--- js/loads.js
+++ js/loads.js
@@ -1,16 +1,8 @@
-function load(URL,ele) {
-    $.ajax({
-        type:'get',
-        dataType:'json',
-        data:{
+function load() {
+    $.getJSON('users.json',function (data) {
+        console.log(data);
+        $.each(data,function (index,element) {
 
-        },
-        url:URL,
-        success:function (data) {
-            //--清空表格
-            $(ele).html('');
-            $.each(data,function (index,element) {
-                //---定义新的一行
                 var newTr = $('<tr></tr>');
                 //---定义用户组
                 var newName = $('<td>'+element.name+'</td>');
@@ -32,199 +24,97 @@ function load(URL,ele) {
                 newTr.append(newAge);
                 newTr.append(newLevel);
                 newTr.append(newEditor);
-                $(ele).append(newTr);
-            });
-            LoadPages();
-//               //----设置删除函数
-            $('.dele').on('click',function () {
-                var $this = $(this);
-                Dele('deleuser.php','showusers.php',$this,ele);
-            });
-            //-----设置锁定函数
-            $('.lock').on('click',function () {
-                var $this = $(this);
-                Suo('lockuser.php','showusers.php',$this,ele);
-            });
-            //-------设置修改密码函数
-            $('.edit').on('click',function () {
-                var $this = $(this);
-                Edit('editpwd.php',$this);
-            })
-        },
-        error:function () {
-            console.log(0);
-        }
-    })
+                $('#lists tbody:eq(0)').append(newTr);
+        });
+        //----分页
+        LoadPages();
+        //----设置锁定函数
+        $('.lock').on('click',function () {
+            var $this = $(this);
+            Suo($this);
+        });
+        //----设置删除函数
+        $('.dele').on('click',function () {
+            var $this = $(this);
+            Dele($this);
+        });
+    });
 }
 function Search() {
     $('#tb tbody').html('');
     var user = $('#username').val();
-    $.ajax({
-        type:'get',
-        dataType:'json',
-        url:'searchuser.php',
-        data:{
-            username:user
-        },
-        success:function (data) {
-            //添加
-            var newTr = $('<tr></tr>');
-            //---定义用户组
-            var newName = $('<td>'+data.name+'</td>');
-            //---定义性别组
-            var newSex = $('<td>'+data.sex+'</td>');
-            //---定义年龄组
-            var newAge = $('<td>'+data.age+'</td>');
-            //---定义等级组
-            var newLevel = $('<td>'+data.juris+'</td>');
-            if(data.juris==1){
-                //---定义操作钮
-                var newEditor = $('<td class="dropdown"><button class="btn btn-default dropdown-toggle" data-toggle="dropdown">操作<span class="caret"></span></button><ul class="dropdown-menu" role="menu"><li class="dele"><a href="###">删除</a></li><li class="lock"><a href="###">锁定</a></li><li><a href="###">修改密码</a></li></ul></td>');
-            }else{
-                var newEditor = $('<td class="dropdown"><button class="btn btn-default dropdown-toggle" data-toggle="dropdown">操作<span class="caret"></span></button><ul class="dropdown-menu" role="menu"><li class="dele"><a href="###">删除</a></li><li class="lock"><a href="###">解锁</a></li><li><a href="###">修改密码</a></li></ul></td>');
+    //----定义查找标志位
+    var flag = 0;
+    //----获取json数据
+    $.getJSON('users.json',function (data) {
+        //---遍历查找相应数据
+        $.each(data,function (index,element) {
+            if(element['name']===user){
+                //----执行该数据添加
+                var newTr = $('<tr></tr>');
+                    //---定义用户组
+                    var newName = $('<td>'+element['name']+'</td>');
+                    //---定义性别组
+                    var newSex = $('<td>'+element['sex']+'</td>');
+                    //---定义年龄组
+                    var newAge = $('<td>'+element['age']+'</td>');
+                    //---定义等级组
+                    var newLevel = $('<td>'+element['juris']+'</td>');
+                    if(element['juris']==1){
+                        //---定义操作钮
+                        var newEditor = $('<td class="dropdown"><button class="btn btn-default dropdown-toggle" data-toggle="dropdown">操作<span class="caret"></span></button><ul class="dropdown-menu" role="menu"><li class="dele"><a href="###">删除</a></li><li class="lock"><a href="###">锁定</a></li><li><a href="###">修改密码</a></li></ul></td>');
+                    }else{
+                        var newEditor = $('<td class="dropdown"><button class="btn btn-default dropdown-toggle" data-toggle="dropdown">操作<span class="caret"></span></button><ul class="dropdown-menu" role="menu"><li class="dele"><a href="###">删除</a></li><li class="lock"><a href="###">解锁</a></li><li><a href="###">修改密码</a></li></ul></td>');
+                    }
+                    //---添加
+                    newTr.append(newName);
+                    newTr.append(newSex);
+                    newTr.append(newAge);
+                    newTr.append(newLevel);
+                    newTr.append(newEditor);
+                    console.log(newTr);
+                    $('#tb tbody').append(newTr);
+                    flag = 1;
             }
-            //---添加
-            newTr.append(newName);
-            newTr.append(newSex);
-            newTr.append(newAge);
-            newTr.append(newLevel);
-            newTr.append(newEditor);
-            console.log(newTr);
-            $('#tb tbody').append(newTr);
-            //----设置删除函数
-            $('.dele').on('click',function () {
-                //---获取用户名
-                var Btr = $(this).parent().parent().parent();
-                var Dname = Btr.children()[0].innerHTML;
-                console.log(Dname);
-                $.ajax({
-                    type:'get',
-                    dataType:'json',
-                    data:{
-                        name:Dname
-                    },
-                    url:'deleuser.php',
-                    success:function (data) {
-                        console.log(data);
-                        Search();
-                    },
-                    error:function () {
-                        console.log(0);
-                    }
-                })
-            });
-            //-----设置锁定函数
-            $('.lock').on('click',function () {
-                //---获取用户名
-                var Btr = $(this).parent().parent().parent();
-                var Dname = Btr.children()[0].innerHTML;
-                $.ajax({
-                    type:'get',
-                    dataType:'json',
-                    data:{
-                        name:Dname
-                    },
-                    url:'lockuser.php',
-                    success:function (data) {
-                        console.log(1);
-                        Search();
-
-                    },
-                    error:function () {
-                        console.log(0);
-                    }
-                })
-            });
-            //-------设置修改密码函数
-            $('.edit').on('click',function () {
-                //---获取用户名
-                var Btr = $(this).parent().parent().parent();
-                var Dname = Btr.children()[0].innerHTML;
-                console.log(Dname);
-                $.ajax({
-                    type:'get',
-                    dataType:'json',
-                    data:{
-                        name:Dname
-                    },
-                    url:'editpwd.php',
-                    success:function (data) {
-                        console.log(data);
-                    },
-                    error:function () {
-                        console.log(0);
-                    }
-                })
-            })
-        },
-        error:function (data) {
+        });
+        if(flag===0){
             alert('查无此人');
         }
+        //----执行锁定函数
+        $('#tb tbody .lock').on('click',function () {
+            var $this = $(this);
+            Suo($this);
+        });
+        //----执行删除函数
+        $('#tb tbody .dele').on('click',function () {
+            var $this = $(this);
+            Dele($this);
+        })
     });
     return false;
+
 }
 //----设定锁定函数
-function Suo(URL1,URL2,ele1,ele2) {
+function Suo(ele1) {
     //---获取用户名
     var Btr = ele1.parent().parent().parent();
-    var Dname = Btr.children()[0].innerHTML;
-    $.ajax({
-        type:'get',
-        dataType:'json',
-        data:{
-            name:Dname
-        },
-        url:URL1,
-        success:function (data) {
-            console.log(111);
-            load(URL2,ele2);
-        },
-        error:function () {
-            console.log(0);
-        }
-    })
+    var Dlevel = Btr.children()[3].innerHTML;
+    if(Dlevel==0){
+        ele1.children()[0].innerHTML = '锁定';
+        Btr.children()[3].innerHTML = 1;
+    }else{
+        ele1.children()[0].innerHTML = '解锁';
+        Btr.children()[3].innerHTML = 0;
+    }
 }
 //----设定删除函数
-function Dele(URL1,URL2,ele1,ele2) {
-    //---获取用户名
-    var Btr = ele1.parent().parent().parent();
-    var Dname = Btr.children()[0].innerHTML;
-    console.log(Dname);
-    $.ajax({
-        type:'get',
-        dataType:'json',
-        data:{
-            name:Dname
-        },
-        url:URL1,
-        success:function (data) {
-            load(URL2,ele2);
-        },
-        error:function () {
-            console.log(0);
-        }
-    })
-}
-//----设定修改密码函数
-function Edit(URL,ele) {
-    //---获取用户名
-    var Btr = ele.parent().parent().parent();
-    var Dname = Btr.children()[0].innerHTML;
-    console.log(Dname);
-    $.ajax({
-        type:'get',
-        dataType:'json',
-        data:{
-            name:Dname
-        },
-        url:URL,
-        success:function (data) {
-            console.log(data);
-        },
-        error:function () {
-            console.log(0);
-        }
-    })
+function Dele(ele1) {
+    //---让表格将该行数据删除
+    //---获取这行
+    var Tr = ele1.parent().parent().parent();
+    Tr.remove();
+    LoadPages();
+
 }
 function SearchUser(ele) {
     //----用于查询
@@ -266,6 +156,7 @@ function LoadPages() {
 }
 //a链接点击变色，再点其他回复原色
 function changCss(obj){
+    // console.log(obj);
     var arr = document.getElementsByTagName("a");
     for(var i=0;i<arr.length;i++){
         if(obj===arr[i]){       //当前页样式
